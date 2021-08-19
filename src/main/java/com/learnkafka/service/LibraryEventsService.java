d903ed411a00b6ac96221696e55494ba99eb5a07
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-08-19 22:59:55 +0530

    Save consumer record

--- src/main/java/com/learnkafka/service/LibraryEventsService.java
+++ src/main/java/com/learnkafka/service/LibraryEventsService.java
@@ -3,6 +3,7 @@ package com.learnkafka.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.entity.LibraryEvent;
+import com.learnkafka.jpa.LibraryEventsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,29 @@ public class LibraryEventsService {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    LibraryEventsRepository libraryEventsRepository;
+
     public void processLibraryEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
-        objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        LibraryEvent libraryEvent = objectMapper.readValue(consumerRecord.value(), LibraryEvent.class);
+        log.info("Library Event : {}", libraryEvent);
+
+        switch (libraryEvent.getLibraryEventType()) {
+            case NEW:
+                // save operation
+                save(libraryEvent);
+                break;
+            case UPDATE:
+                // update operation
+                break;
+            default:
+                log.info("invalid Library Event Type");
+        }
+    }
+
+    private void save(LibraryEvent libraryEvent) {
+        libraryEvent.getBook().setLibraryEvent(libraryEvent);
+        libraryEventsRepository.save(libraryEvent);
+        log.info("Successfully Persisted the library Event {}", libraryEvent);
     }
 }
